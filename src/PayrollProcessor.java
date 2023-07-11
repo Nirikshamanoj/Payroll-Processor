842585847e0bed3d556187f3818cf18467e1daa3
Niriksha M <88819709+Nirikshamanoj@users.noreply.github.com>
2023-07-11 12:07:22 +0530

    Update PayrollProcessor.java

--- src/PayrollProcessor.java
+++ src/PayrollProcessor.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PayrollProcessor {
+import java.util.*;
+public class PayrollProcessor
+{
+    static void calculatePay(String name,double salaryPerDay)
+    {
+        double salary=salaryPerDay*30;
+        System.out.println("Salaried Employee");
+        System.out.println("Name:"+name+"Salary:"+salary);
+    }
+    static void calculatePay(String name,double hourlyRate,double hoursWorked)
+    {
+        double salary=hourlyRate*hoursWorked;
+        System.out.println("Hourly Employee");
+        System.out.println("Name:"+name+"Salary:"+salary);
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        int salaryPerDay=scanner.nextInt();
+        String name=scanner.next();
+        double hourlyRate=scanner.nextDouble();
+        double hoursWorked=scanner.nextDouble();
+        calculatePay(name,salaryPerDay);
+        calculatePay(name,hourlyRate,hoursWorked);
+    }
 }
+
